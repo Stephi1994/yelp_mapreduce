ce4811eb424167545e95697352b7c947a2345a6d
Stephi1994 <nicole1494@hotmail.com>
2016-03-13 17:31:16 -0500

    para leer los comentarios

--- Datos_yelp.py
+++ Datos_yelp.py
@@ -8,7 +8,7 @@ def some_action(post):
 
 
 access_token = 'CAACEdEose0cBAJWAUXodZB7pGUdZBkZBnOPOvEylGPaUUXvjr7pZC6V7ieZCU6CzCZBRgfJxz28SzOQ5H63L2qQvCnFFV2r7j7CsZCVdryqCtnUdZAD4ofhaE6uZB8so2A92b6JxE39iZCC5iInrLLZBTlOhkqdMRsFZBsp9retr46NjPLxRbrJqyHLhwz5QsVNup4xBsDXg0A40wGbVjbsG1z73iIuwsJpmzj0ZD'
-user = 'Stephanie'
+user = 'StephanieP'
 
 graph = yelp_academic_dataset_review.GraphAPI(access_token)
 profile = graph.get(user)
