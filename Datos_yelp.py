71df4e34772344af74ccd0c29c0207b4ca4cb3df
Stephi1994 <nicole1494@hotmail.com>
2016-03-13 17:28:25 -0500

    Added files via upload

---
+++ Datos_yelp.py
@@ -0,0 +1,25 @@
+import yelp_academic_dataset_review
+import requests
+
+
+def some_action(post):
+     print(post['created_time'])
+
+
+
+access_token = 'CAACEdEose0cBAJWAUXodZB7pGUdZBkZBnOPOvEylGPaUUXvjr7pZC6V7ieZCU6CzCZBRgfJxz28SzOQ5H63L2qQvCnFFV2r7j7CsZCVdryqCtnUdZAD4ofhaE6uZB8so2A92b6JxE39iZCC5iInrLLZBTlOhkqdMRsFZBsp9retr46NjPLxRbrJqyHLhwz5QsVNup4xBsDXg0A40wGbVjbsG1z73iIuwsJpmzj0ZD'
+user = 'Stephanie'
+
+graph = yelp_academic_dataset_review.GraphAPI(access_token)
+profile = graph.get(user)
+posts = graph.get(profile['id'], 'posts')
+
+while True:
+    try:
+        [some_action(post=post) for post in posts['data']]
+
+        posts = requests.get(posts['paging']['next']).json()
+    except KeyError:
+
+        break
+
